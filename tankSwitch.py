fa4970d7f22a39c04ce3ebba22168b4999c125f7
Jamie <jamieharwood007@gmail.com>
2018-04-02 19:57:40 +0100

    Basic switch input

--- tankSwitch.py
+++ tankSwitch.py
@@ -1,8 +1,25 @@
 import machine
 import time
 
-def main():
-    functionSelect = 0
-    waterOn = 0
+functionSelect = 0
+functionSelectLast = -1
+waterOn = 0
+waterOnLast = -1
     
-main()
+functionSelectPin = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
+waterOnPin = machine.Pin(1, machine.Pin.IN, machine.Pin.PULL_UP)
+powerLedPin = machine.Pin(3, machine.Pin.OUT)
+
+powerLedPin.on()
+   
+while True:
+    functionSelect = functionSelectPin.value()
+    waterOn = waterOnPin.value()
+    
+    if ( functionSelect != functionSelectLast ):
+        functionSelectLast = functionSelect
+        
+    if ( waterOn != waterOnLast ):
+        waterOnLast = waterOn
+
+    time.sleep(0.25)
