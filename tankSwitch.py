1d79cd7534c04b1a6bfd8ef0fd7f8c0bde12ff75
Jamie <jamieharwood007@gmail.com>
2018-04-05 22:47:37 +0100

    Added an additional function LED

--- tankSwitch.py
+++ tankSwitch.py
@@ -9,28 +9,32 @@ functionSelectPin = Pin(0, Pin.IN, Pin.PULL_UP)  # D3
 waterOnPin = Pin(2, Pin.IN, Pin.PULL_UP)  # D4
 
 powerLedPin = Pin(4, Pin.OUT)  #D2
-functionLedPin = Pin(5, Pin.OUT)  #D1
+functionIrrigationLedPin = Pin(5, Pin.OUT)  #D1
+functionHoseLedPin = Pin(14, Pin.OUT)  #D5
 waterLedPin = Pin(15, Pin.OUT)  # D8
 
 def main():
     np = neopixel.NeoPixel(machine.Pin(12), 4)
-    
+
     # Set initial state
     np[0] = (255,  0,  0)
     powerLedPin.on()
-    functionLedPin.off()
+    functionIrrigationLedPin.off()
+    functionHoseLedPin.off()
     waterLedPin.off()
-    
+
     vars.functionSelect = functionSelectPin.value()
     vars.waterOn = waterOnPin.value()
-    
+
     if (vars.functionSelect == True):
         np[1] = (0,  0,  0)
-        functionLedPin.off()
+        functionIrrigationLedPin.on()
+        functionHoseLedPin.off()
     else:
         np[1] = (0,  255,  0)
-        functionLedPin.on()
-    
+        functionIrrigationLedPin.off()
+        functionHoseLedPin.on()
+
     if (vars.waterOn == True):
         np[2] = (0,  0,  0)
         waterLedPin.off()
@@ -39,25 +43,24 @@ def main():
         waterLedPin.on()
 
     while True:
-        # utime.sleep_ms(100)
-    
-        #print('Button state change, pin ',  p)
         # Read switch inputs
         vars.functionSelect = functionSelectPin.value()
         vars.waterOn = waterOnPin.value()
-    
+
         # Check against the last input
         if ( vars.functionSelect != vars.functionSelectLast ):
             if (vars.functionSelect == True):
                 np[1] = ( 0,  0,  0)
-                functionLedPin.off()
+                functionIrrigationLedPin.on()
+                functionHoseLedPin.off()
             else:
                 np[1] = ( 0,  255,  0)
-                functionLedPin.on()
-    
+                functionIrrigationLedPin.off()
+                functionHoseLedPin.on()
+
             vars.functionSelectLast = vars.functionSelect  # Set the last pointers
             print('if ( functionSelect != functionSelectLast ):')
-    
+
         if ( vars.waterOn != vars.waterOnLast ):
             if (vars.waterOn == True):
                 np[2] = ( 0,  0,  0)
@@ -65,7 +68,7 @@ def main():
             else:
                 np[2] = ( 0,  255,  0)
                 waterLedPin.on()
-    
+
             vars.waterOnLast = vars.waterOn  # Set the last pointers
             print('if ( waterOn != waterOnLast ):')
 
@@ -73,5 +76,3 @@ def main():
         utime.sleep_ms(200)
 
 main()
-
-
